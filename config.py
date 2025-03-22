cd625a5a1ae8223b83117b019997321610918488
utkarsh764 <utkarshsrivastavalala123@gmail.com>
2025-03-22 18:01:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -82,7 +82,7 @@ FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {first}\n\n<b>ᴊ�
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @nova_flix</b>")
 
 #set True if you want to prevent users from forwarding files from bot
-PROTECT_CONTENT = False if os.environ.get('PROTECT_CONTENT', "False") == "False" else False
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else True
 
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
